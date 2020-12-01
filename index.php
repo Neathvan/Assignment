e7e6c4a9148ffa4575692c0e2ed663f96203bc74
Neathvan <70628068+Neathvan@users.noreply.github.com>
2020-12-02 06:51:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,2 @@
-<?php include_once("task1.html") ?>
-<?php include_once("task2.html") ?>
-<?php include_once("task3.html") ?>
-<?php include_once("task4.html") ?>
+
 <?php include_once("render_tasks.html") ?>
